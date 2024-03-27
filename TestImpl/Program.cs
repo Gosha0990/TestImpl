9fa554a0cab629f0aec9412079dcb0928db6190c
Георгий Барышников <bo0909@live.ru>
2024-03-27 09:07:05 +0300

    Добавлена проверка плагинов и воспроизведение ошибки при отсутствие плагина

--- TestImpl/Program.cs
+++ TestImpl/Program.cs
@@ -8,4 +8,21 @@ var pluginFactory = new Plugins();
 var pluginSum = pluginFactory.GetPlugin("PluginSum");
 
 Console.WriteLine(pluginSum.Run(1, 1));
+
+var pluginMul = pluginFactory.GetPlugin("PluginMultiplication");
+
+Console.WriteLine(pluginMul.Run(2, 4));
+
+var pluginSubtract = pluginFactory.GetPlugin("PluginSubtract");
+
+Console.WriteLine(pluginSubtract.Run(9, 5));
+
+var pluginDiv = pluginFactory.GetPlugin("PluginDivision");
+
+Console.WriteLine(pluginDiv.Run(6, 5));
+
+var pluginError = pluginFactory.GetPlugin("test");
+
+Console.WriteLine(pluginError.Run(1,1));
+
 Console.ReadLine();
